a09e688b9c94307497086632f03a2412a0981631
vickKaren <Oliveiravittoria13@gmail.com>
2021-09-09 18:20:20 +0000

    opa

---
+++ hooks/src/views/contents/NaoEncontrado.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+import './NaoEncontrado.css'
+const NaoEncontrado = props => (
+    <div className="erro">
+        <h1>Página não encontrada ;-;</h1>
+    </div>
+
+
+)
+export default NaoEncontrado
